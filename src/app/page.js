641ea1a7d8eef5878da297ce6d57375c5cdaea84
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-25 12:21:58 +0100

    Add state management and effect hook to HomePage component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,8 @@
+"use client";
+import { useState, useEffect } from "react";
 export default function HomePage() {
+  const [queryResult, setqueryResult] = useState();
+  useEffect(() => {}, []);
   return (
     <main className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-200 dark:from-gray-900 dark:to-gray-800 text-gray-900 dark:text-white px-6 py-10">
       <header className="mb-6">
